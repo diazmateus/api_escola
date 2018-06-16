0280e347b2b261cfcf98a2ee468008c67cbb7c23
Mateus Diaz <diazmateus@hotmail.com>
2018-06-15 23:01:21 -0300

    Adicionado Servidor Remoto

--- config/db_config.js
+++ config/db_config.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
-var urlString = 'mongodb://localhost/API_T';
+var urlString = 'mongodb://super:senha1702@ds163300.mlab.com:63300/thy8795l';
+// var urlString = 'mongodb://localhost/API_T';
 
 mongoose.connect(urlString, function (erro, res){
     if (erro){
